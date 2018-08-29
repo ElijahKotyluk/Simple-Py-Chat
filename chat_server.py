30b068cca67c848ba31ea58632d8a4dbeb2ec500
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-08-29 06:24:43 -0700

    Add server function for handling client.

--- chat_server.py
+++ chat_server.py
@@ -15,6 +15,7 @@ SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
 ''' While loop that waits for incoming connections, logs connection when successful. '''
+''' ln27: Store clients address in dictionary, then starts the handling thread for that client. '''
 def accept_incoming_connections():
     ''' Handle incoming requests from clients '''
     while True:
@@ -24,3 +25,23 @@ def accept_incoming_connections():
         "Type your screen name and press enter.",
         "utf8"))
         addresses[client] = client_addressThread(target=handle_client, args=(client,)).start()
+
+''' Takes client socket as arg. '''
+def handle_client(client):
+    ''' Handles single client connection. '''
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you want to quit, type {quit} to exit.' % name
+    client.send(bytes,(msg, "utf8"))
+    msg = "%s has joined the chat." % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+    while True:
+        msg = client.recv[BUFSIZ]
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
